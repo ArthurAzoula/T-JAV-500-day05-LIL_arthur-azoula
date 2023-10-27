c8cb165d4d4cd7e3aae7e2ebab5d6ad00cab2c02
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-27 14:44:07 +0200

    add exercice 6

--- AssaultTerminator.java
+++ AssaultTerminator.java
@@ -1,2 +1,20 @@
-public class AssaultTerminator {
+public class AssaultTerminator extends SpaceMarine {
+
+    protected String name;
+
+    protected AssaultTerminator(String name) {
+        super(name, 150, 30);
+        this.name = name;
+        System.out.println(this.name + " has teleported from space.");
+        super.equip(new PowerFist());
+    }
+
+    @Override
+    public void receiveDamage(int damage) {
+        damage -= 3;
+        if (damage < 1) {
+            damage = 1;
+        }
+        super.receiveDamage(damage);
+    }
 }
--- TacticalMarine.java
+++ TacticalMarine.java
@@ -6,7 +6,14 @@ public class TacticalMarine extends SpaceMarine {
         super(name, 100, 20);
         this.name = name;
         System.out.println(this.name + " on duty.");
-
+        super.equip(new PlasmaRifle());
     }
 
+    @Override
+    public void recoverAP() {
+        this.ap += 12;
+        if (this.ap > 50) {
+            this.ap = 50;
+        }
+    }
 }
